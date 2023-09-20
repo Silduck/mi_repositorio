e74b0a47c65ecad4e61decd6b277adf3925fa8fe
grupossg <silgisela12@gmail.com>
2023-09-20 16:15:22 -0300

    cambios recientes

--- NEGOCIO2/MiProyecto/app/views.py
+++ NEGOCIO2/MiProyecto/app/views.py
@@ -18,28 +18,28 @@ from .forms import BuscarProductosForm
 
 
 def mostrar_sucursales (request):
-    s1 = sucursales(direccion='Hipolito Yrigoyen 1325', cuit=2569874521036, telefono=2204836985, instagram= '@mundoshoes', localidad='Merlo')
-    s2 = sucursales(direccion='av.LIbertador 196', cuit=305896321, telefono=305896321, instagram= '@mundoshoes', localidad='Merlo') 
-    s3 = sucursales(direccion='Perón 1157', cuit=32213554654, telefono=54544456, instagram= '@mundoshoes',localidad='Moron') 
+    s1 = sucursales(direccion='Hipolito Yrigoyen 1325', cuit=2569874521036, telefono=2204836985, instagram= '@calzadoslospatos', localidad='Merlo')
+    s2 = sucursales(direccion='av.LIbertador 196', cuit=305896321, telefono=2203058963, instagram= '@calzadoslospatos', localidad='Merlo') 
+    s3 = sucursales(direccion='Perón 1157', cuit=32213554654, telefono=2205454445, instagram= '@calzadoslospatos',localidad='Moron') 
 
 
     return render(request, 'app/sucursales.html',{'sucursales':[s1,s2,s3]})
 
 
 def mostrar_proveedores (request):
-    p1 = proveedores(nombre='Santiago', apellido='Cardozo', telefono=45683892, direccion= 'cetra 489') 
-    p2 = proveedores(nombre='juan', apellido='Perez', telefono=49873292, direccion= 'avellaneda 4089')
-    p3 = proveedores(nombre='Gustavo', apellido='Capdevila', telefono=48678992, direccion= 'kiernan 5989')
-    p4 = proveedores(nombre='Horacio', apellido='Lopez', telefono=39565892, direccion= 'Uruguay 489')
+    p1 = proveedores(nombre='Santiago', apellido='Cardozo', telefono=4568389, direccion= 'Sullivan 489') 
+    p2 = proveedores(nombre='juan', apellido='Perez', telefono=4987329, direccion= 'Avellaneda 4089')
+    p3 = proveedores(nombre='Gustavo', apellido='Capdevila', telefono=2204867899, direccion= 'Guardia vieja 1980')
+    p4 = proveedores(nombre='Horacio', apellido='Lopez', telefono=3956589, direccion= 'Uruguay 489')
 
 
     return render(request, 'app/proveedores.html',{'proveedores':[p1,p2,p3,p4]})
 
 def mostrar_personalLocal (request):
-    pl1 = personalLocal(nombre='Diego', apellido='Daro', edad=45, telefono= 1564592356) 
-    pl2 = personalLocal(nombre='Ignacio', apellido='Perez', edad=54, telefono= 1566891235) 
-    pl3 = personalLocal(nombre='Laura', apellido='Martiniano', edad=25, telefono= 1565517886) 
-    pl4 = personalLocal(nombre='Ivanna', apellido='Gonzalez', edad=35, telefono= 1556324589) 
+    pl1 = personalLocal(nombre='Diego', apellido='Daro', edad=45, telefono= 1164592356) 
+    pl2 = personalLocal(nombre='Ignacio', apellido='Perez', edad=54, telefono= 1166891235) 
+    pl3 = personalLocal(nombre='Laura', apellido='Martiniano', edad=25, telefono= 1165517886) 
+    pl4 = personalLocal(nombre='Ivanna', apellido='Gonzalez', edad=35, telefono= 1156324589) 
 
 
     return render(request, 'app/personalLocal.html',{'personalLocal':[pl1,pl2,pl3,pl4]})
@@ -100,6 +100,19 @@ def buscar_articulo(request):
     else:
         respuesta = 'No hay datos'
     return render(request, 'app/buscar_productos.html', {'respuesta': respuesta})
+
+def eliminar_cliente(request, cliente_id):
+
+    cliente = cliente.objects.get(id-cliente_id)
+
+    cliente.delete() 
+
+    cliente = cliente.objects.all()
+
+    context = {'clientes':clientes}
+
+    return render(request, 'app/clientes.html', context=context)
+
 
 
 
